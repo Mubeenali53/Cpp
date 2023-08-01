15ab39b1ecf591f6ec713218b0e7a062238add9a
Syed Mubeen Ali <95215270+Mubeenali53@users.noreply.github.com>
2023-08-01 18:34:55 +0530

    Create Sum of Digits.cpp

---
+++ Sum of Digits.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int x,y=0;
+    cin>>x;
+    while(x=!0)
+    {
+        y=y+x%10;
+        x=x/10;
+    }
+    cout<<y;
+    return 0;
+}
