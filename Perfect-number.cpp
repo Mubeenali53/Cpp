1b07cdec8f3dfe1ab72bd9f7dd4ff4c37a896ce4
Syed Mubeen Ali <95215270+Mubeenali53@users.noreply.github.com>
2023-10-10 12:50:26 +0530

    Create Perfect-number.cpp

---
+++ Perfect-number.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+ int main()
+  {
+    int t,n,sum=0;
+    cin>>t;
+    while(t--){
+      cin>>n;
+      for(int i=1;i<n;i++){
+        if(n%i==0){
+          sum+=i;
+        }
+      }
+      if(sum==n){
+        cout<<"true"<<endl;
+      }
+      else{
+        cout<<"false"<<endl;
+      }
+    }
+    return 0;
+  }
