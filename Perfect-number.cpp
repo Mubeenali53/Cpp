c4cd760eed376091cde117d67f16292f5c42c036
Syed Mubeen Ali <95215270+Mubeenali53@users.noreply.github.com>
2023-10-10 13:00:09 +0530

    Update Perfect-number.cpp

--- Perfect-number.cpp
+++ Perfect-number.cpp
@@ -2,10 +2,11 @@
 using namespace std;
  int main()
   {
-    int t,n,sum=0;
+    int t,n,sum;
     cin>>t;
     while(t--){
       cin>>n;
+     sum=0;
       for(int i=1;i<n;i++){
         if(n%i==0){
           sum+=i;
