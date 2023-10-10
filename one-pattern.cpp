f0fb325cc8c7e35bf3358bdf5b638c7dbec463fc
Syed Mubeen Ali <95215270+Mubeenali53@users.noreply.github.com>
2023-10-10 11:07:41 +0530

    Create one-pattern.cpp

---
+++ one-pattern.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+int main()
+  {
+   int N,i,j;
+   cin>>N;
+   for(i=0;i<=4;i++)
+   {
+   j=i;
+   for(j=0;j<=i;j++)
+   {
+   cout<<N;
+     
+   }
+   
+   cout<<endl;
+     
+   }
+    return 0;
+  }
