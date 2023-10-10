b79949bbe078b85730243b60a10ec61997d4ab1d
Syed Mubeen Ali <95215270+Mubeenali53@users.noreply.github.com>
2023-10-10 13:54:32 +0530

    Create operators.cpp

---
+++ operators.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main()
+  {
+    int x,y;
+    cin>>x>>y;
+    if(x<y)
+     cout<<x<<" is smaller than "<<y;
+     if(x>y)
+     cout<<x<<" is greater than "<<y;
+     if(x=y)
+     cout<<x<<" is equal "<<y;
+    return 0;
+  }
